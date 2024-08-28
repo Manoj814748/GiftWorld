376061f8446e118ff3b5068ffc06b1e2a1489045
Manoj B Madivalar <manojmadivalar123@gmail.com>
2024-08-28 12:47:06 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   return (
     <div className='header-container'>
-    <div className='header'>
+    <div className='header1'>
     <h1></h1>
     <SearchBar />
     {/* Your content or list goes here */}
